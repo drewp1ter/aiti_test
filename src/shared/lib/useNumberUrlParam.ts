9d35a2ca4a17c0bc446c839075a3aad1847a5a65
drewp1ter <belora2010@yandex.ru>
2026-04-15 00:25:21 +0300

    fix build issue

--- src/shared/lib/useNumberUrlParam.ts
+++ src/shared/lib/useNumberUrlParam.ts
@@ -1,14 +1,14 @@
 'use client'
 
 import { useCallback, useState } from 'react'
-import { useSearchParams } from 'next/navigation'
+import { isServer } from './isServer'
 
 function isParamInvalid(param: number) {
 	return param < 1 || !Number.isInteger(param) || isNaN(param)
 }
 
 export function useNumberUrlParam(paramName: string, defaultValue: number): [number, (newPage: number) => void] {
-	const searchParams = useSearchParams()
+	const searchParams = isServer() ? new URLSearchParams() : new URLSearchParams(window.location.search)
 	const searchParam = searchParams.get(paramName)
 	const [param, setParam] = useState(() => {
 		const parsedParam = Number(searchParam)
@@ -17,7 +17,7 @@ export function useNumberUrlParam(paramName: string, defaultValue: number): [num
 
 	const setParamValue = useCallback(
 		(newPage: number) => {
-			const params = new URLSearchParams(searchParams.toString())
+			const params = new URLSearchParams(window.location.search)
 			params.set(paramName, String(newPage))
 			const newUrl = `${window.location.pathname}${params.size > 0 ? '?' + params.toString() : ''}`
 			window.history.replaceState({}, '', newUrl) // не используем роутер, чтобы не ререндерить всю страницу
