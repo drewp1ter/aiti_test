5d5a8a89e2046350d5aacbcfc4ceb5beed7bf73a
drewp1ter <belora2010@yandex.ru>
2026-04-15 12:06:13 +0300

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 export const dynamic = 'force-dynamic'
 import { SessionGuard } from '@/shared/ui'
+import Link from 'next/link'
 
 export default async function Page() {
 	return (
 		<SessionGuard>
-			<div />
+			<div className='p-5 flex flex-col gap-4'>
+				<h1>Home Page</h1>
+				<p>Welcome to the home page!</p>
+				<Link href="/products">Go to Products</Link>
+			</div>
 		</SessionGuard>
 	)
 }
